a0162ed993c0605456d3d91046af92077ae05381
Deep Sen <dsen6072@gmail.com>
2025-06-23 13:14:47 +0530

    Initial commit

---
+++ admin/index.php
@@ -0,0 +1,91 @@
+<?php
+// include 'menu.php';
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dashboard</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<style>
+html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
+</style>
+</head>
+<body class="w3-light-grey">
+
+<!-- Top container -->
+<div class="w3-bar w3-top w3-black w3-large" style="z-index:4">
+  <button class="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey" onclick="w3_open();"><i class="fa fa-bars"></i>  Menu</button>
+  <span class="w3-bar-item w3-right">ElectroShop</span>
+</div>
+
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar  w3-collapse w3-white w3-animate-left" style="z-index:1;width:300px;box-shadow:1px 1px 1px 1px grey" id="mySidebar"><br>
+  <div class="w3-container w3-row">
+    <div class="w3-col s4">
+      <img src="../assets/images/icons/electroshop.png" class="w3-circle w3-margin-right" style="width:64px">
+    </div>
+    <div class="w3-col s8 w3-bar">
+      <span>Welcome, <strong>User</strong></span><br>
+      
+    </div>
+  </div>
+
+  <div class="w3-container">
+    <h5>Dashboard</h5>
+  </div>
+  <div class="w3-bar-block">
+    <a href="#" class="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onclick="w3_close()" title="close menu"><i class="fa fa-remove fa-fw"></i>  Close Menu</a>
+    <a href="./" class="w3-bar-item w3-button w3-padding w3-blue"> <i class="fa-solid fa-users"></i>  Overview</a>
+    <a href="client.html" class="w3-bar-item w3-button w3-padding"> <i class="fa-solid fa-user"></i>  My clients</a>
+    <a href="order.html" class="w3-bar-item w3-button w3-padding"><i class="fa-solid fa-cart-shopping"></i> Orders</a>
+    <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa-solid fa-right-from-bracket"></i> Logout</a><br><br>
+  </div>
+</nav>
+
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:256px;margin-top:43px;">
+
+ 
+  
+
+ 
+</div>
+
+<script>
+// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
+
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
+
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function w3_open() {
+  if (mySidebar.style.display === 'block') {
+    mySidebar.style.display = 'none';
+    overlayBg.style.display = "none";
+  } else {
+    mySidebar.style.display = 'block';
+    overlayBg.style.display = "block";
+  }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+  mySidebar.style.display = "none";
+  overlayBg.style.display = "none";
+}
+</script>
+
+</body>
+</html>
